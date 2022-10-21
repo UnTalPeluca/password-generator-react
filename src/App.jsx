6447497ad6daafe482613b9dfe2aef258977e077
Rodrigo Vergara <67773102+UnTalPeluca@users.noreply.github.com>
2022-10-21 06:28:36 -0300

    Update background to gradient

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import PasswordGenerator from "./components/PasswordGenerator/PasswordGenerator"
 
 const App = () => {
   return (
-    <div className="bg-very-dark-grey min-h-screen min-w-full text-almost-white flex place-items-center">
+    <div className="bg-gradient-to-b from-[#14131b] to-[#08070b] min-h-screen min-w-full text-almost-white flex place-items-center">
       <div className="text-center w-11/12 mx-auto max-w-xs sm:max-w-sm">
         <h1 className="text-grey font-bold pb-4 text-2xl">Password Generator</h1>
         <PasswordGenerator />
